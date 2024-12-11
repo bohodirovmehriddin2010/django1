87ebb26711eeb86ad4da984d54a19a8d65568355
mehriddin <mehriddinbohodirov0@gmail.com>
2024-12-11 15:27:35 +0500

    change

---
+++ home/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=20)
+    description = models.TextField()
+    image = models.ImageField(upload_to='post/')
+    create_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Post
+
+
+
+def home(request):
+    post = Post.objects.all()
+    context = {
+        'post' : post
+    }
+    return render(request, 'index.html', context)
